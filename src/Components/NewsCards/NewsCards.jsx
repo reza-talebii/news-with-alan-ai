833b5fe04380b1a25d16b1226c6310c867142e54
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-13 13:11:20 +0430

    add helper card

--- src/Components/NewsCards/NewsCards.jsx
+++ src/Components/NewsCards/NewsCards.jsx
@@ -4,26 +4,16 @@ import { Grid, Grow, Typography } from "@material-ui/core";
 import NewsCard from "../NewsCard/NewsCard";
 import useStyles from "./styles.js";
 
+
+
 const NewsCards = ({ articles, activeArticle }) => {
   const classes = useStyles();
 
-  // if (!articles.length) {
-  //   return (
-  //     <Grow in>
-  //       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-  //         {infoCards.map((infoCard) => (
-  //           <Grid item xs={12} sm={6} md={4} lg={3} className={classes.infoCard}>
-  //             <div className={classes.card} style={{ backgroundColor: infoCard.color }}>
-  //               <Typography variant="h5" component="h5">{infoCard.title}</Typography>
-  //               {infoCard.info ? <Typography variant="h6" component="h6"><strong>{infoCard.title.split(' ')[2]}</strong>: <br />{infoCard.info}</Typography> : null}
-  //               <Typography variant="h6" component="h6">Try saying: <br /> <i>{infoCard.text}</i></Typography>
-  //             </div>
-  //           </Grid>
-  //         ))}
-  //       </Grid>
-  //     </Grow>
-  //   );
-  // }
+  if (!articles.length) {
+    return (
+     
+    );
+  }
 
   return (
     <Grow in>
