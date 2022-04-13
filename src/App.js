45861c5e4641beab2b1d51a9e00639c99aecd0ba
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-13 09:44:35 +0430

    add alan btn key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
-  return <div className="App"></div>;
-}
+const ALAN_KEY =
+  "0a33d16502ee9e0f70085dcd43bfced22e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+const App = () => {
+  useEffect(() => {
+    alanBtn({
+      key: ALAN_KEY,
+      onCommand: ({ command }) => {
+        if (command === "test") {
+          alert("ok");
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>alan news</h1>
+    </div>
+  );
+};
 
 export default App;
