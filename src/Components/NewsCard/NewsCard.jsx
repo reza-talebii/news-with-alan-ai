de454034d2b76a190371b8828cd2e36ca0d58d9d
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-13 12:41:22 +0430

    styled newsCard

--- src/Components/NewsCard/NewsCard.jsx
+++ src/Components/NewsCard/NewsCard.jsx
@@ -1,8 +1,67 @@
 import React from "react";
 
-const NewsCard = ({ article }) => {
-  console.log(article);
-  return <div>NewsCard</div>;
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  activeArticle,
+  i,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={activeArticle === i ? classes.activeCard : classes.card}>
+      <CardActionArea href={url} target="_blank">
+        <CardMedia
+          className={classes.media}
+          image={
+            urlToImage ||
+            "https://www.industry.gov.au/sites/default/files/August%202018/image/news-placeholder-738.png"
+          }
+          title={title}
+        />
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {new Date(publishedAt).toDateString()}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="h2">
+            {source.name}
+          </Typography>
+        </div>
+        <Typography
+          className={classes.title}
+          gutterBottom
+          variant="h5"
+          component="h2"
+        >
+          {title}
+        </Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" href={url}>
+          Learn More
+        </Button>
+        <Typography variant="h5" color="textSecondary" component="h2">
+          {i + 1}
+        </Typography>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default NewsCard;
