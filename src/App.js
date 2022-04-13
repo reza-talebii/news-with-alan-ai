311ee0febd3660a69c7faae0d6bd7ab702d44d48
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-13 12:20:08 +0430

    add  news cards file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 const ALAN_KEY =
   "0a33d16502ee9e0f70085dcd43bfced22e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: ALAN_KEY,
-      onCommand: ({ command }) => {
-        if (command === "test") {
-          alert("ok");
-        }
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadLines") setNewsArticles(articles);
       },
     });
   }, []);
