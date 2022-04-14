05a8fe52ba21bf720cb6181b8ed8a9b09f1a340a
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-14 16:13:12 +0430

    end project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import wordsToNumbers from "words-to-numbers";
 import NewsCards from "./Components/NewsCards/NewsCards";
 import Header from "./Components/Header/Header";
 import Loading from "./Components/Loading/Loading";
+// import Error from "./Components/Alert/Error";
 
 const ALAN_KEY =
   "0a33d16502ee9e0f70085dcd43bfced22e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -73,6 +74,7 @@ const App = () => {
   return (
     <div>
       {loading && <Loading />}
+      {error && <h5>فیلتر شکنتو روشن کن</h5>}
       <Header newsArticles={newsArticles} />
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
