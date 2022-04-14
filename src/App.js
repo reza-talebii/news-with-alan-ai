c2434d0b114aaf65a4557eeefd94a822c2fa4987
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-04-14 14:56:58 +0430

    styled loading

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import wordsToNumbers from "words-to-numbers";
 
 import NewsCards from "./Components/NewsCards/NewsCards";
 import Header from "./Components/Header/Header";
+import Loading from "./Components/Loading/Loading";
 
 const ALAN_KEY =
   "0a33d16502ee9e0f70085dcd43bfced22e956eca572e1d8b807a3e2338fdd0dc/stage";
@@ -12,6 +13,7 @@ const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
   const [activeArticle, setActiveArticle] = useState(-1);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   //HANDLING ALAN
   useEffect(() => {
@@ -69,6 +71,7 @@ const App = () => {
 
   return (
     <div>
+      {loading && <Loading />}
       <Header newsArticles={newsArticles} />
       <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
